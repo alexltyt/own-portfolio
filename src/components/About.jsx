69a9c520f9ccc674b9261d0527902ed3d9db3086
alexltyt <alexltytca@gmail.com>
2023-06-02 22:04:35 -0600

    fixed top margin prolem

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,24 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
-        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-            <div className="pb-8">
-                <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-                    About
-                </p>
-            </div>
-            <p className="text-xl mt-20 ">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia a quidem dicta eveniet modi officia omnis hic at. Est consectetur iure voluptatem totam debitis. Cum nisi, labore et soluta numquam reprehenderit praesentium similique, facilis excepturi quis, in optio eligendi veniam. Sunt, iusto iste ipsa id officiis maxime asperiores deserunt minima.
-            </p>
-            <br/>
-            <p className="text-xl">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi perferendis optio qui est facere adipisci officia eaque cum harum inventore, nulla dolores doloribus obcaecati ex asperiores corporis quis deleniti tempora a ullam assumenda commodi repellendus suscipit minima? Provident nam, quos laborum, quidem officiis animi culpa dolores repudiandae nisi a rerum.
-            </p>
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
         </div>
+        <p className="text-xl mt-20 ">
+          Based in Calgary from 2022, full time studying Software Development
+          Diploma in SAIT, will graduate on April 2024. Equiped Python, C#,
+          JavaScript, HTML, CSS, at SAIT's learning, have so much passion on
+          software, web and mobile app development. With 13 years Banking
+          industry experience, expert in selling and customer relationship.
+          Founded Sunbath Store (online grocery store) and JEFT (Herbal tea
+          manufacture) in 2021. Passion in music and sport, over 20 years
+          taekwondo couching experience and also singer-songwriter for
+          Canton-pop music.
+        </p>
+        <br />
+        {/* <p className="text-xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+          perferendis optio qui est facere adipisci officia eaque cum harum
+          inventore, nulla dolores doloribus obcaecati ex asperiores corporis
+          quis deleniti tempora a ullam assumenda commodi repellendus suscipit
+          minima? Provident nam, quos laborum, quidem officiis animi culpa
+          dolores repudiandae nisi a rerum.
+        </p> */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,11 +5,11 @@ import { Link } from 'react-scroll'
 
 const Home = () => {
   return (
-    <div name="home" className="h-screen w-full bg-gradient-to-b from-black to-gray-800">
-      <div className="max-w-screen-lg ms-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-        <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Future Full Stack Developer
+    <div name="home" className="mt-10 h-screen w-full bg-gradient-to-b from-black to-gray-800 ">
+      <div className=" ms-auto flex flex-col items-center h-full justify-center px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full ">
+          <h2 className=" md:text-7xl font-bold text-white w-fit text-3xl  ">
+            I'm a Full Stack Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
           Enthusiastic and highly motivated software development student with a 4.0 GPA, passionate about building innovative software solutions. Equipped with soft skills such as communication, problem-solving, and the ability to work collaboratively in a team setting. Demonstrated leadership and organizational skills as the Initiator and Vice President of the Web Development Club, successfully hosting full-house MERN workshops. Won second place in the P-Hack 2022 Hackathon at SAIT
@@ -24,7 +24,7 @@ const Home = () => {
           </div>
         </div>
         <div>
-          <img src={heroImage} alt="my profile" className="rounded-2xl mx-auto w-2/3 md:w-full"/>
+          <img src={heroImage} alt="my profile" className="rounded-2xl mx-auto w-1/3 md:w-full"/>
         </div>
       </div>
     </div>
