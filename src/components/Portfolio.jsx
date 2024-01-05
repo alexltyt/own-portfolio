b7a5736df46d702a789931bee7024be1c3c62df4
alexltyt <alexltytca@gmail.com>
2024-01-04 22:30:36 -0700

    Add new projects to portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,12 +5,15 @@ import portfolioWeb from "../assets/portfolio/portfolioWeb.png";
 import sniperAcademy from "../assets/portfolio/sniperAcademy.png";
 import xpert from "../assets/portfolio/Xpertucantwin.png";
 import wordsOfWesteros from "../assets/portfolio/wordsOfWesteros.png";
+import snapsettle from "../assets/portfolio/snapsettle.png";
 
 const Portfolio = () => {
   const portfolio = [
     {
       id: 1,
       src: sunbathgamescore,
+      title: "Sunbath Gamescore",
+      description: "Score tracking and timer for Rummikub player",
       showDemo: true,
       demoURL: "https://alexltyt.github.io/SunbathGamescore/",
       gitURL:"https://github.com/alexltyt/SunbathGamescore"
@@ -18,25 +21,33 @@ const Portfolio = () => {
     {
       id: 2,
       src: xpert,
+      title: "Xpert You Can't Win",
+      description: "Tic tac toe game with an unbeatable AI",
       showDemo: false,
       gitURL:"https://github.com/alexltyt/XpertYouCantWin"
     },
     {
       id: 3,
-      src: wordsOfWesteros,
+      src: snapsettle,
+      title: "Snapsettle",
+      description: "Simplify the process of settling bills",
       showDemo: true,
-      demoURL: "https://words-of-westeros-alexltyt.vercel.app/",
-      gitURL:"https://github.com/alexltyt/WordsofWesteros"
+      demoURL: "https://www.sunbathsoft.com/snapsettle/",
+      gitURL:"https://github.com/alexltyt/snapsettle"
     },
     {
       id: 4,
       src: sniperAcademy,
+      title: "Sniper Academy",
+      description: "Sniper game built with Unity",
       showDemo: false,
       gitURL:"https://github.com/alexltyt/sniperAcademy"
     },
     {
       id: 5,
       src: decidophobia,
+      title: "Decidophobia",
+      description: "Decision making app for indecisive people",
       showDemo: true,
       demoURL: "https://alexltyt.github.io/Decidophobia/",
       gitURL:"https://github.com/alexltyt/Decidophobia"
@@ -44,8 +55,19 @@ const Portfolio = () => {
     {
       id: 6,
       src: portfolioWeb,
+      title: "Portfolio Website",
+      description: "What you are seeing right now",
       showDemo: false,
       gitURL:"https://github.com/alexltyt/own-portfolio"
+    },
+    {
+      id: 7,
+      src: wordsOfWesteros,
+      title: "Words of Westeros",
+      description: "Game of Thrones quote generator",
+      showDemo: true,
+      demoURL: "https://words-of-westeros-alexltyt.vercel.app/",
+      gitURL:"https://github.com/alexltyt/WordsofWesteros"
     },
       
   ];
@@ -61,26 +83,34 @@ const Portfolio = () => {
           </p>
         </div>
         <div  className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-        {portfolio.map(({ id, src,showDemo,demoURL,gitURL }) => (
+        {portfolio.map(({ id, src,title,description,showDemo,demoURL,gitURL }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <p className="text-center text-sm p-1 tracking-wider">
+                {title}
+                </p>
               <img
                 src={src}
                 alt=""
                 className="rounded-md duration-200 hover:scale-105"
               />
-              <div className="flex items-center justify-center">
-                {showDemo&& (
-                <button 
-                  className=" w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
-                  onClick={() => window.open(demoURL, '_blank')}>
-                  Demo
-                </button>
-                )}
-                <button 
-                  className=" w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
-                  onClick={() => window.open(gitURL, '_blank')}>
-                  GitHub
-                </button>
+              <div>
+                <p className="text-center text-sm p-1">
+                  {description}
+                </p>
+                <div className="flex items-center justify-center">
+                  {showDemo&& (
+                  <button 
+                    className=" w-1/2 px-6 py-3 mx-4 duration-200 underline hover:scale-105"
+                    onClick={() => window.open(demoURL, '_blank')}>
+                    Demo
+                  </button>
+                  )}
+                  <button 
+                    className=" w-1/2 px-6 py-3 mx-4 duration-200 underline hover:scale-105"
+                    onClick={() => window.open(gitURL, '_blank')}>
+                    GitHub
+                  </button>
+                </div>
               </div>
             </div>
         ))}
